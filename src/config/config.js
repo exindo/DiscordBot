d01acc12c322e8733aaf11f6b96665d4ccd7ab66
exindo <78916453+exindo@users.noreply.github.com>
2022-05-20 14:55:44 +0700

    Update config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,8 +1,8 @@
 const dotenv = require("dotenv").config({ path: __dirname + "/./../../.env" })
 
 module.exports = {
-  TOKEN: process.env.token,
-  PREFIX: process.env.prefix,
+  TOKEN: "OTc2NzI0NzAzNzEwNDI1MTE4.GU-9wJ.i6gAbhsBcUTKmIEVUmhesX9zgvEMd22dy4ZQF0",
+  PREFIX: ".",
   OWNER: process.env.owner,
   CORONA_API: "https://corona.lmao.ninja/v2",
 }
